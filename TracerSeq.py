ca469a6d0117de814bb464260a68f07f33634e94
danwagnerlab <38510695+wagnerde@users.noreply.github.com>
2023-11-17 11:46:01 -0800

    Update TracerSeq.py

--- TracerSeq.py
+++ TracerSeq.py
@@ -19,7 +19,7 @@ def process_tracerseq_csv_to_counts(run_params):
 
     # Configure some global settings
     matplotlib.rcParams['figure.dpi'] = 300
-    matplotlib.rcParams['figure.figsize'] = [5,5]
+    matplotlib.rcParams['figure.figsize'] = [4,4]
     warnings.simplefilter(action='ignore', category=FutureWarning)
 
     # Step 1: Import the entire set of TracerSeq CSV file(s) into a dataframe, appending library prefices to CB columns
